32b2fcbb04ada817ceb6e94fe17a73840eab855a
David Fernadez <daffevafernandez@gmail.com>
2024-06-05 21:17:21 +0200

    date and hour

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
+let date = document.getElementById("date")
+let hora = document.getElementById("hora")
 let power = document.getElementById("power");
 let encendido = false;
+
+let datebutton = 1;
+let fecha = new Date();
 
 power.addEventListener("click", () => {
     power.classList.toggle("ON")
     encendido = !encendido
-   
+    if (datebutton == 1) {
+        hora.style.visibility = "none";
+        date.style.visibility = "none";
+        hora.innerHTML = fecha.toLocaleTimeString();
+        date.innerHTML = fecha.toLocaleDateString();
+        setTimeout(function () {
+            hora.style.visibility = "hidden";
+            date.style.visibility = "hidden";
+        }, 4500);
+    }
+  
 })
+
