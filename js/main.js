fc95b0bd991f075a3654fd2125352d2a227d3321
David Fernadez <daffevafernandez@gmail.com>
2024-06-05 21:34:35 +0200

    channels implemented in the title

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-let date = document.getElementById("date")
-let hora = document.getElementById("hora")
 let power = document.getElementById("power");
 let encendido = false;
 
-let datebutton = 1;
+let boton = document.getElementsByClassName("boton");
+let arrayBoton = Array.from(boton)
+
+let date = document.getElementById("date")
+let hora = document.getElementById("hora")
 let fecha = new Date();
+
+
+
+for(let i = 0; i < arrayBoton.length; i++){
+    arrayBoton[i].addEventListener("click", () => {
+        if(encendido){
+            canal.innerHTML = `Canal ${i+1}`
+        }
+    })
+}
 
 power.addEventListener("click", () => {
     power.classList.toggle("ON")
     encendido = !encendido
-    if (datebutton == 1) {
-        hora.style.visibility = "none";
-        date.style.visibility = "none";
-        hora.innerHTML = fecha.toLocaleTimeString();
-        date.innerHTML = fecha.toLocaleDateString();
-        setTimeout(function () {
-            hora.style.visibility = "hidden";
-            date.style.visibility = "hidden";
-        }, 4500);
-    }
-  
+
+    hora.innerHTML = fecha.toLocaleTimeString();
+    date.innerHTML = fecha.toLocaleDateString();
+
+    setTimeout(function () {
+        hora.style.visibility = "hidden";
+        date.style.visibility = "hidden";
+    }, 4500);
 })
 
